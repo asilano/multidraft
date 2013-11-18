1f028881a0325464283a7eda17bfab27b27c89ec
Chris Howlett <chowlett09+github@gmail.com>
2013-11-18 13:33:30 +0000

    Initial creation of Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Multidraft::Application.config.secret_token = '495284e76aee2b39d8baa16f94f3fd303428341cba8c7aa3ab158234685502f3011e45287b45f2f59efc77f48daee2913f2bab1ccf3f3315b89b1d830fa639dc'
