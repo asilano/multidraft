4d3df44b0c3138e9cc12f1f4226eb7bd04afb982
Chris Howlett <chowlett09+github@gmail.com>
2013-12-02 13:27:08 +0000

    Vague nod to responsive styling

--- spec/requests/user_registrations_spec.rb
+++ spec/requests/user_registrations_spec.rb
@@ -4,5 +4,6 @@ describe "UserRegistrations" do
   it "is linked from the homepage" do
     visit '/'#root_path
     expect(page).to have_link('Sign up')
+    expect(page).to have_link('Sign in')
   end
 end
